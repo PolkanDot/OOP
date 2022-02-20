98e8a7cceb95685ccb7dee045d626bb588a80ef1
PolkanDot <insarmiftahov@yandex.ru>
2022-02-20 19:06:31 +0300

    Переработал тесты на использование нужных тестовых данных

--- 1lab/ReplaceProject/Test.bat
+++ 1lab/ReplaceProject/Test.bat
@@ -18,19 +18,21 @@ FC.exe test_input_output\empty.txt "%TEMP%\empty_output.txt" > nul
 if ERRORLEVEL 1 GOTO error
 ECHO Test 2 successful
 
-REM Work with empty file
-%program% test_input_output\input.txt "%TEMP%\output.txt" g u
+REM Work with mormal args
+%program% test_input_output\g_input.txt "%TEMP%\output.txt" g u
 if ERRORLEVEL 1 GOTO error
-FC.exe test_input_output\output.txt "%TEMP%\output.txt" > nul 
+FC.exe test_input_output\g_output.txt "%TEMP%\output.txt" > nul 
 if ERRORLEVEL 1 GOTO error
 ECHO Test 3 successful
 
-REM Work with empty file
-%program% test_input_output\input.txt "%TEMP%\empty_output.txt" a b
+REM Work with wrong substrings
+%program% test_input_output\g_input.txt "%TEMP%\output.txt" a b
 if ERRORLEVEL 1 GOTO error
-FC.exe test_input_output\input.txt "%TEMP%\empty_output.txt" > nul 
+FC.exe test_input_output\g_input.txt "%TEMP%\output.txt" > nul 
 if ERRORLEVEL 1 GOTO error
 ECHO Test 4 successful
+
+
 
 REM Tests were successful
 ECHO All tests were successful
