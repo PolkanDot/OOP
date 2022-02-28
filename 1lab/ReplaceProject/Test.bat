facaab451ac67f0d4a793d4609ce2e7884860940
PolkanDot <insarmiftahov@yandex.ru>
2022-02-28 10:51:37 +0300

    Исправил успешный вывод тестов на более понятный

--- 1lab/ReplaceProject/Test.bat
+++ 1lab/ReplaceProject/Test.bat
@@ -9,28 +9,28 @@ IF program == "" (
 REM При запуске без параметров ожидается ненулевой код возврата
 %program% >nul
 IF NOT ERRORLEVEL 1 GOTO error
-ECHO Test 1 successful
+ECHO Test without parameters successful
 
 REM Work with empty file
 %program% test_input_output\empty.txt "%TEMP%\empty_output.txt" a b
 if ERRORLEVEL 1 GOTO error
 FC.exe test_input_output\empty.txt "%TEMP%\empty_output.txt" > nul 
 if ERRORLEVEL 1 GOTO error
-ECHO Test 2 successful
+ECHO Test with empty file successful
 
-REM Work with mormal args
+REM Work with normal args
 %program% test_input_output\g_input.txt "%TEMP%\output.txt" g u
 if ERRORLEVEL 1 GOTO error
 FC.exe test_input_output\g_output.txt "%TEMP%\output.txt" > nul 
 if ERRORLEVEL 1 GOTO error
-ECHO Test 3 successful
+ECHO Test with normal args successful
 
 REM Work with wrong substrings
 %program% test_input_output\g_input.txt "%TEMP%\output.txt" a b
 if ERRORLEVEL 1 GOTO error
 FC.exe test_input_output\g_input.txt "%TEMP%\output.txt" > nul 
 if ERRORLEVEL 1 GOTO error
-ECHO Test 4 successful
+ECHO Test with wrong substrings successful
 
 
 REM Work with empty findingString
@@ -38,21 +38,21 @@ REM Work with empty findingString
 if ERRORLEVEL 1 GOTO error
 FC.exe test_input_output\g_input.txt "%TEMP%\output.txt" > nul 
 if ERRORLEVEL 1 GOTO error
-ECHO Test 5 successful
+ECHO Test empty findingString successful
 
 REM Work with same substrings ("ma" "mama")
 %program% test_input_output\ma_input.txt "%TEMP%\output.txt" ma mama 
 if ERRORLEVEL 1 GOTO error
 FC.exe test_input_output\ma_output.txt "%TEMP%\output.txt" > nul 
 if ERRORLEVEL 1 GOTO error
-ECHO Test 6 successful
+ECHO Test with same substrings successful
 
 REM Work with same substrings and text
 %program% test_input_output\123_input.txt "%TEMP%\output.txt" 1231234 w 
 if ERRORLEVEL 1 GOTO error
 FC.exe test_input_output\123_output.txt "%TEMP%\output.txt" > nul 
 if ERRORLEVEL 1 GOTO error
-ECHO Test 7 successful
+ECHO Test with same substrings and text successful
 
 REM Tests were successful
 ECHO All tests were successful
